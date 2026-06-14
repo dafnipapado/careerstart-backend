d91c802f9be5b4541dcc7b4bcc9fa1c3334c4ff9
DaphnePap <daphnepapadop@gmail.com>
2026-06-14 19:21:25 +0300

    initial commit

---
+++ src/test/java/io/github/dafnipapado/careerstart_backend/CareerstartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dafnipapado.careerstart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerstartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
