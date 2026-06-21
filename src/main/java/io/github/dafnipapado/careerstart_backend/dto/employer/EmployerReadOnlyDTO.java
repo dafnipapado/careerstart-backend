819aa797762595925c2e04decab86df2765d6431
DaphnePap <daphnepapadop@gmail.com>
2026-06-21 23:58:32 +0300

    feat: add EmployerReadOnlyDTO

---
+++ src/main/java/io/github/dafnipapado/careerstart_backend/dto/employer/EmployerReadOnlyDTO.java
@@ -0,0 +1,8 @@
+package io.github.dafnipapado.careerstart_backend.dto.employer;
+
+public record EmployerReadOnlyDTO(
+        String uuid,
+        String brandName,
+        String username
+) {
+}
