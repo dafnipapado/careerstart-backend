873b33895ac3e95976d43779056d30d6710741e2
DaphnePap <daphnepapadop@gmail.com>
2026-06-23 01:15:30 +0300

    feat: add findByUuid query method

--- src/main/java/io/github/dafnipapado/careerstart_backend/repository/EmployerRepository.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/repository/EmployerRepository.java
@@ -4,7 +4,10 @@ import io.github.dafnipapado.careerstart_backend.model.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface EmployerRepository extends JpaRepository<Employer, Long> {
+    Optional<Employer> findByUuid(UUID uuid);
     Optional<Employer> findByVat(String vat);
+
 }
