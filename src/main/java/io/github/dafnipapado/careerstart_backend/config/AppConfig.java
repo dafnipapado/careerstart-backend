fbd0de580f5f45ad876b95d9058b2401c71198cd
DaphnePap <daphnepapadop@gmail.com>
2026-06-27 23:44:58 +0300

    feat: add app configuration

---
+++ src/main/java/io/github/dafnipapado/careerstart_backend/config/AppConfig.java
@@ -0,0 +1,14 @@
+package io.github.dafnipapado.careerstart_backend.config;
+
+import org.apache.tika.Tika;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Tika tika() {
+        return new Tika();
+    }
+}
