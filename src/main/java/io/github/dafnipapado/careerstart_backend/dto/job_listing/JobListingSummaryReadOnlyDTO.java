c19fcaa391fab13c1d2d364b50d3b2783c062929
DaphnePap <daphnepapadop@gmail.com>
2026-07-04 22:25:38 +0300

    feat: add JobListingSummaryReadOnlyDTO

---
+++ src/main/java/io/github/dafnipapado/careerstart_backend/dto/job_listing/JobListingSummaryReadOnlyDTO.java
@@ -0,0 +1,11 @@
+package io.github.dafnipapado.careerstart_backend.dto.job_listing;
+
+public record JobListingSummaryReadOnlyDTO(
+        String uuid,
+        String title,
+        String regionName,
+        String professionalFieldName,
+        String dateCreated,
+        String employerBrandName
+) {
+}
