2f6cb54f10b29c5e08d4994a3afbffc8e4b14597
DaphnePap <daphnepapadop@gmail.com>
2026-07-08 17:06:52 +0300

    refactor: add EntityGraph to findByUsername

--- src/main/java/io/github/dafnipapado/careerstart_backend/repository/UserRepository.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package io.github.dafnipapado.careerstart_backend.repository;
 
 import io.github.dafnipapado.careerstart_backend.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"role", "role.capabilities"})
     Optional<User> findByUsername(String username);
     Optional<User> findByRoleId(Long id);
 
