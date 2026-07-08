2fabfb069d7cb8930b538813b32c820c5c519a62
DaphnePap <daphnepapadop@gmail.com>
2026-07-08 18:44:52 +0300

    refactor: change authentication endpoint

--- src/main/java/io/github/dafnipapado/careerstart_backend/security/SecurityConfig.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.POST, "/api/v1/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/employers").hasAuthority("INSERT_EMPLOYER")
                         .requestMatchers(HttpMethod.PUT, "/api/v1/employers/{uuid}").hasAuthority("UPDATE_EMPLOYER")
                         .requestMatchers(HttpMethod.PATCH, "/api/v1/employers/{uuid}").hasAuthority("DELETE_EMPLOYER")
