e0ec5e94f2b4d8ed660bd30ecaf978c1b76240b2
DaphnePap <daphnepapadop@gmail.com>
2026-08-31 23:51:11 +0300

    feat: add getCurrentUser service method

--- src/main/java/io/github/dafnipapado/careerstart_backend/service/IUserService.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/service/IUserService.java
@@ -1,8 +1,10 @@
 package io.github.dafnipapado.careerstart_backend.service;
 
 import io.github.dafnipapado.careerstart_backend.core.exception.EntityNotFoundException;
+import io.github.dafnipapado.careerstart_backend.model.User;
 import io.github.dafnipapado.careerstart_backend.model.static_data.Role;
 
 public interface IUserService {
     Role getRoleByName(String roleName) throws EntityNotFoundException;
+    User getCurrentUser();
 }
--- src/main/java/io/github/dafnipapado/careerstart_backend/service/UserServiceImpl.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package io.github.dafnipapado.careerstart_backend.service;
 
 import io.github.dafnipapado.careerstart_backend.core.exception.EntityNotFoundException;
+import io.github.dafnipapado.careerstart_backend.model.User;
 import io.github.dafnipapado.careerstart_backend.model.static_data.Role;
 import io.github.dafnipapado.careerstart_backend.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +20,10 @@ public class UserServiceImpl implements IUserService{
     public Role getRoleByName(String roleName) throws EntityNotFoundException {
         return roleRepository.findByName(roleName)
                 .orElseThrow(() -> new EntityNotFoundException("Role", "Role with name = {" + roleName + "} not found."));
+    }
+
+    @Override
+    public User getCurrentUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
