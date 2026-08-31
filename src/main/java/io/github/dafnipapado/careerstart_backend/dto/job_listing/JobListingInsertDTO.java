12a02c205a9a1a02adca86f7aeb5b49a9aba7ba2
DaphnePap <daphnepapadop@gmail.com>
2026-09-01 00:55:07 +0300

    refactor: use logged-in employer instead of employer from dto

--- src/main/java/io/github/dafnipapado/careerstart_backend/dto/job_listing/JobListingInsertDTO.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/dto/job_listing/JobListingInsertDTO.java
@@ -14,9 +14,6 @@ public record JobListingInsertDTO(
     String description,
 
     @NotNull
-    Long employerId,
-
-    @NotNull
     Long professionalFieldId,
 
     @NotNull
--- src/main/java/io/github/dafnipapado/careerstart_backend/service/JobListingServiceImpl.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/service/JobListingServiceImpl.java
@@ -4,7 +4,6 @@ import io.github.dafnipapado.careerstart_backend.core.exception.EntityNotFoundEx
 import io.github.dafnipapado.careerstart_backend.dto.job_listing.*;
 import io.github.dafnipapado.careerstart_backend.filters.JobListingFilters;
 import io.github.dafnipapado.careerstart_backend.mapper.Mapper;
-import io.github.dafnipapado.careerstart_backend.model.Employer;
 import io.github.dafnipapado.careerstart_backend.model.JobListing;
 import io.github.dafnipapado.careerstart_backend.model.static_data.ProfessionalField;
 import io.github.dafnipapado.careerstart_backend.model.static_data.Region;
@@ -37,22 +36,22 @@ public class JobListingServiceImpl implements IJobListingService{
     private final RegionRepository regionRepository;
     private final IEmployerService employerService;
     private final IPersonalInfoService personalInfoService;
+    private final IUserService userService;
 
     @Override
     @Transactional(rollbackFor = EntityNotFoundException.class)
     public JobListingReadOnlyDTO save(JobListingInsertDTO jobListingInsertDTO) throws EntityNotFoundException {
         JobListing jobListing = mapper.mapToJobListingEntity(jobListingInsertDTO);
 
-        Employer employer = employerRepository.findById(jobListingInsertDTO.employerId())
-                .orElseThrow(() -> new EntityNotFoundException("Employer", "Employer not found."));
-        jobListing.setEmployer(employer);
+        jobListing.setEmployer(userService.getCurrentUser().getEmployer());
+
         ProfessionalField professionalField = employerService.getProfessionalFieldById(jobListingInsertDTO.professionalFieldId());
         jobListing.setProfessionalField(professionalField);
         Region region = personalInfoService.getRegionById(jobListingInsertDTO.regionId());
         jobListing.setRegion(region);
 
         jobListingRepository.save(jobListing);
-        log.info("Job listing {{}} with uuid = {{}} by employer = {{}} was saved successfully.", jobListing.getTitle(), jobListing.getUuid(), employer.getBrandName());
+        log.info("Job listing {{}} with uuid = {{}} by employer = {{}} was saved successfully.", jobListing.getTitle(), jobListing.getUuid(), jobListing.getEmployer().getBrandName());
 
         return mapper.mapToJobListingReadOnlyDTO(jobListing);
     }
