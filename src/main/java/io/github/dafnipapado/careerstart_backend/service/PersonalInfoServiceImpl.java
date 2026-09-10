99b64ca650368cb869a08201d2844c77b54db999
DaphnePap <daphnepapadop@gmail.com>
2026-09-10 19:01:42 +0300

    refactor: remove unnecessary method from personalInfo service

--- src/main/java/io/github/dafnipapado/careerstart_backend/service/PersonalInfoServiceImpl.java
+++ src/main/java/io/github/dafnipapado/careerstart_backend/service/PersonalInfoServiceImpl.java
@@ -1,7 +1,6 @@
 package io.github.dafnipapado.careerstart_backend.service;
 
 import io.github.dafnipapado.careerstart_backend.core.exception.EntityNotFoundException;
-import io.github.dafnipapado.careerstart_backend.model.PersonalInfo;
 import io.github.dafnipapado.careerstart_backend.model.static_data.Region;
 import io.github.dafnipapado.careerstart_backend.repository.PersonalInfoRepository;
 import io.github.dafnipapado.careerstart_backend.repository.RegionRepository;
@@ -15,17 +14,10 @@ import org.springframework.stereotype.Service;
 public class PersonalInfoServiceImpl implements IPersonalInfoService {
 
     private final RegionRepository regionRepository;
-    private final PersonalInfoRepository personalInfoRepository;
 
     @Override
     public Region getRegionById(Long regionId) throws EntityNotFoundException {
         return regionRepository.findById(regionId)
                 .orElseThrow(() -> new EntityNotFoundException("Region", "Region with id = {" + regionId + "} not found."));
-    }
-
-    @Override
-    public PersonalInfo getPersonalInfoByEmail(String email) throws EntityNotFoundException {
-        return personalInfoRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("PersonalInfo", "Personal Info with email = {" + email + "} not found."));
     }
 }
